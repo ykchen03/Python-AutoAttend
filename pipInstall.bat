c42eaf8bec6e9b9439011f3fe01b5f93f301e391
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2022-10-18 09:55:24 +0800

    Add files via upload

---
+++ pipInstall.bat
@@ -0,0 +1,19 @@
+@echo off
+color 0A
+echo Installation start!
+python.exe -m pip install --upgrade pip
+
+pip install PyScreeze
+
+pip install pywin32
+
+pip install keyboard
+
+pip install pyautogui
+
+pip install opencv-python
+
+pip install Pillow --upgrade
+
+echo Done!
+pause
