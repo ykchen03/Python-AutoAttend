fd76c5f982b97f413d2114c53f0d6e987d814af1
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2022-10-18 09:56:12 +0800

    Add files via upload

---
+++ 1920x1080/AutoIRS/AutoIRS.py
@@ -0,0 +1,23 @@
+﻿import pyautogui
+import keyboard
+import time
+
+print("Time:",time.strftime("%H:%M:%S", time.localtime()))
+print("Start in 5 sec...")
+time.sleep(5)
+print('Start')
+
+while True:
+    cord=pyautogui.locateOnScreen("target.PNG",region=(870,870,1030,945),grayscale=True,confidence=0.5)
+    
+    if cord!=None:
+        print("Found at",time.strftime("%H:%M:%S", time.localtime()))
+        pyautogui.click(cord)
+        break
+        
+    else:
+        print("Not Found Next Check in 30 sec")
+        keyboard.send("F5")
+    time.sleep(30)
+
+
---
+++ 1920x1080/AutoMoodle/AutoMoodle.py
@@ -0,0 +1,21 @@
+﻿import pyautogui
+import keyboard
+import time
+
+print("Time:",time.strftime("%H:%M:%S", time.localtime()))
+print("Start in 5 sec...")
+time.sleep(5)
+print('Start')
+
+while True:
+    cord=pyautogui.locateOnScreen("target.PNG",region=(45,390,110,445),grayscale=True,confidence=0.5)
+
+    if cord!=None:
+        print("Found at",time.strftime("%H:%M:%S", time.localtime()))
+        pyautogui.click(cord)
+        
+    else:
+        print("Not Found Next Check in 30 sec")
+        keyboard.send("F5")
+    time.sleep(30)
+
