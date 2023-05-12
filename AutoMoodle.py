1790b1b887b788019fd2a32154de4f205d2bbebd
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2023-05-12 11:26:57 +0800

    Update AutoMoodle.py

--- AutoMoodle.py
+++ AutoMoodle.py
@@ -1,4 +1,4 @@
-﻿import configparser
+import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -23,15 +23,18 @@ driver.get(driver.find_element(By.XPATH, f"//*[contains(@title,'{config.get('Con
 driver.implicitly_wait(5)
 
 driver.get(driver.find_element(By.XPATH, "//*[contains(@href,'attendance')]").get_attribute('href'))
-
+#https://moodle.chu.edu.tw/mod/attendance/attendance.php?sessid=1503919&amp;sesskey=yaDkjaHTV3
 driver.implicitly_wait(5)
 
-#https://moodle.chu.edu.tw/mod/attendance/attendance.php?sessid=1503919&amp;sesskey=yaDkjaHTV3
+print("課名:"+driver.find_element(By.TAG_NAME,"h1").text)
+
 print("Moodle自動點名已啟動 待命中...")
 while True:
     
     if driver.find_elements(By.XPATH,"//*[contains(@href,'sessid')]"):
         driver.get(driver.find_element(By.XPATH, "//*[contains(@href,'sessid')]").get_attribute('href'))
+        print("點名完成 3秒後自動關閉")
+        time.sleep(3)
         break
     
     #print('Not Found')
