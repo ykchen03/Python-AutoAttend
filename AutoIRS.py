308f46b6f8684b85918de0d1be16ff7452a93369
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2023-05-12 11:24:21 +0800

    Update AutoIRS.py

--- AutoIRS.py
+++ AutoIRS.py
@@ -1,4 +1,4 @@
-﻿import configparser
+import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -20,14 +20,17 @@ driver.implicitly_wait(5)
 
 course_id = driver.find_element(By.XPATH, f"//*[contains(text(),'{config.get('Config','classCode')}')]").get_attribute('data-course-id')
 
-driver.execute_script(f"irs_currentRollcall({course_id})")
+driver.execute_script(f"irs_currentRollcall('{course_id}')")
 
 driver.implicitly_wait(5)
 
+print("課名:"+driver.find_element(By.CLASS_NAME,"back-button-title").text)
 print("IRS自動點名已啟動 待命中...")
 while True:
-    if driver.find_elements(By.XPATH,"//div[@class='text' and text()='開放簽到中']"):
-        driver.execute_script(f"makeRollcall({course_id})")
+    if driver.find_elements(By.XPATH,"//div[@class='text' and text()='開放簽到中']"):#目前未開放簽到 #開放簽到中
+        driver.execute_script(f"makeRollcall('{course_id}')")
+        print("點名完成 3秒後自動關閉")
+        time.sleep(3)
         break
 
     #print("Not Found")
