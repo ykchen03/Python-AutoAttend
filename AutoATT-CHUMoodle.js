1c9f31032d5cff8a8f31bd8da5abd14ae3a59a1f
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2023-06-01 11:28:13 +0800

    Add files via upload

---
+++ AutoATT-CHUMoodle.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         AutoATT-CHUMoodle
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       YK
+// @match        https://moodle.chu.edu.tw/mod/attendance/view.php?id=*
+// @icon         https://moodle.chu.edu.tw/theme/image.php/chu/theme/1685412700/favicon
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    window.addEventListener('load', function() {
+        var title = document.createElement('h1');
+        title.textContent = "自動點名啟動中";
+        title.style.color='green';
+        document.getElementsByClassName("page-header-headings")[0].appendChild(title);
+        var button = document.getElementsByClassName("btn btn-primary");
+        for(var i of button)
+        {
+            if(i.hasAttribute("href"))
+            {
+                console.log(i);
+                window.location.href=i.href;
+                clearInterval(refreshInterval);
+                title.textContent = "點名完成";
+                return;
+            }
+        }
+    });
+
+    const refreshInterval = setInterval(function(){
+        location.reload();
+    },15000);
+
+    // Your code here...
+})();
---
+++ AutoATT-ZuvioIRS.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         AutoATT-ZuvioIRS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       YK
+// @match        https://irs.zuvio.com.tw/student5/irs/rollcall/*
+// @icon         https://s3.hicloud.net.tw/zuvio.public/public/system/images/irs_v4/favicon/student5.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    window.addEventListener('load', function() {
+       var title = document.getElementsByClassName("text")[0];
+        title.textContent = "自動點名啟動中";
+        title.style.color = "MediumSeaGreen";
+        title.style.fontSize = "30px";
+        var button = document.getElementById("submit-make-rollcall");
+        if(button)
+        {
+            button.click();
+            clearInterval(refreshInterval);
+            title.textContent = "點名完成";
+            return;
+        }
+    });
+
+    const refreshInterval = setInterval(function(){
+        location.reload();
+    },15000);
+
+    // Your code here...
+})();
