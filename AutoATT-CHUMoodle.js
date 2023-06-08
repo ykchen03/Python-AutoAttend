472727e22185fe4428e9bb0db041f4e7e8934f29
ykchen03 <112570539+ykchen03@users.noreply.github.com>
2023-06-08 16:25:04 +0800

    Update AutoATT-CHUMoodle.js

--- AutoATT-CHUMoodle.js
+++ AutoATT-CHUMoodle.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoATT-CHUMoodle
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       YK
 // @match        https://moodle.chu.edu.tw/mod/attendance/view.php?id=*
