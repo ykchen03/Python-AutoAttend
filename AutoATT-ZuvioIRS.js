b3e7d78fa0c5cef3ef26f9742610c3802c895e7a
ykchen03 <ykchen03@gmail.com>
2023-10-17 11:03:23 +0800

    Update AutoATT-ZuvioIRS.js

--- AutoATT-ZuvioIRS.js
+++ AutoATT-ZuvioIRS.js
@@ -6,21 +6,15 @@
 // @author       YK
 // @match        https://irs.zuvio.com.tw/student5/irs/rollcall/*
 // @icon         https://s3.hicloud.net.tw/zuvio.public/public/system/images/irs_v4/favicon/student5.ico
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @grant        none
 // ==/UserScript==
-/*global $*/
-$().ready(function() {
-	console.log("ready");
-	$("#content .irs-rollcall .text").text("自動點名啟動中").css({
-		"color": "green",
-		"font-weight": "bold",
-		"font-size": "40px"
-	});
-	if ($("#submit-make-rollcall").length > 1) {
-		new Function($("#submit-make-rollcall").attr("onclick"))();
-	}
-	setTimeout(function() {
-		window.location.reload();
-	}, 12000);
-});
+document.querySelector("#content .irs-rollcall .text").textContent = "自動點名啟動中";
+setTimeout(function() {
+    console.log(rollcall_id);
+    if (rollcall_id != '') {
+        makeRollcall(rollcall_id);
+    }
+}, 3000);
+setTimeout(function() {
+    window.location.reload();
+}, 12000);
